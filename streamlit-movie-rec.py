43a3b932e172dc245bc5c99da571af720fced685
Akshay Bahadur <akshaybahadur@Akshays-MacBook-Pro-2.local>
2022-03-11 00:17:41 -0500

    restructured the changes

--- streamlit-movie-rec.py
+++ streamlit-movie-rec.py
@@ -5,26 +5,31 @@ import streamlit as st
 
 st.set_page_config(page_title='BigDL Movie Recommendation System',
                    layout='wide')
+
+st.image("https://bigdl-project.github.io/img/bigdl_logo.png", width=400)
 df = pd.read_csv("resources/cache.csv")
 
 st.write("""
 # BigDL Movie Recommendation System
 In this implementation, we have used **bigDL** framework for training a movie recommendation system.
-Movie recommendations have been cached
 """)
 
 st.subheader('BigDL')
 st.write("""
 BigDL is a distributed deep learning library for Apache Spark; with BigDL, users can write their deep learning applications as standard Spark programs, which can directly run on top of existing Spark or Hadoop clusters.
 """)
+
 if st.button('Press to generate insights from data'):
     st.markdown('**1.1. Glimpse of the cached Recommendations**')
-    st.dataframe(df)
+    st.table(df.head(2))
 
-    st.markdown('**1.2. Number of unique users**')
+    st.markdown('**1.2. Layers of Neural Network**')
+    st.image("resources/layers.png", width=1200)
+
+    st.markdown('**1.3. Number of unique users**')
     st.info(len(df))
 
-    st.markdown('**1.3. Number of unique Movies**')
+    st.markdown('**1.4. Number of unique Movies**')
     movie_list = []
 
 
@@ -36,10 +41,10 @@ if st.button('Press to generate insights from data'):
     df.apply(lambda x: f(x), axis=1)
     st.info(len(movie_list))
 
-    st.markdown('**1.4. Sample Movie Names**')
+    st.markdown('**1.5. Sample Movie Names**')
     st.info((movie_list)[0:10])
 
-    st.markdown('**1.5. Number of unique Genres**')
+    st.markdown('**1.6. Number of unique Genres**')
     set_genre = set()
 
 
@@ -50,7 +55,7 @@ if st.button('Press to generate insights from data'):
 
     df.apply(lambda x: f(x), axis=1)
     st.info(len(set_genre))
-    st.markdown('**1.6. Sample Genre Names**')
+    st.markdown('**1.7. Sample Genre Names**')
     st.info((list(set_genre))[0:10])
 
 with st.sidebar.header('1. Select UserID'):
@@ -69,21 +74,30 @@ if st.sidebar.button('Press to generate recommendations'):
     st.info("Generating recommendations for userID : " + str(userID))
     filtered_df = df[df['user'] == userID]
 
+
     def f(x):
         return ast.literal_eval(x['name'])
+
+
     filtered_df['name'] = filtered_df.apply(lambda x: f(x), axis=1)
 
 
     def f(x):
         return ast.literal_eval(x['Genre'])
+
+
     filtered_df['Genre'] = filtered_df.apply(lambda x: f(x), axis=1)
 
     res_list = []
+
+
     def f(x):
         for i, n in enumerate(x['Genre']):
             for g in genres:
                 if g in n:
                     res_list.append(x['name'][i])
+
+
     if genres != None and len(genres) > 0:
         filtered_df.apply(lambda x: f(x), axis=1)
         st.table(res_list[0:num_rec])
